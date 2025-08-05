1d672225caea8c34989dbf15a19c8e10d562fd51
dogg0-hub <konnichidoggo@gmail.com>
2025-08-05 22:18:09 +0900

    フォーム追加

--- studyauthjs/src/app/dashboard/page.tsx
+++ studyauthjs/src/app/dashboard/page.tsx
@@ -1,14 +1,28 @@
 'use client'
 import { signOut } from "next-auth/react"
+import { Chocolate_Classical_Sans } from "next/font/google";
 
 export default function Home(){
     const handleLogout = async() =>{
         signOut({callbackUrl : "/", redirect:true });
     }
+
+    async function applyHandler(){
+        await alert("ボタンが押されました")
+        //postを送る
+        //route.tsでpostを受け取る
+        //fetch使う
+    }
+
     return(
         <div>Dashboard<br />
             ログインしたら入れる画面<br />
             <button onClick={handleLogout} className="galaxy">サインアウトする</button>
+            <form action="post">
+                <p>入力欄</p>
+                <input type="text" className="galaxy-input"/><br/>
+                <button onClick={()=>applyHandler()} className="galaxy">POST</button>
+            </form>
         </div>
     )
 }
