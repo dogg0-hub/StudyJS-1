800232e12c87d741ff1040342157e72e0032762f
dogg0-hub <konnichidoggo@gmail.com>
2025-08-13 00:01:49 +0900

    日付変わる前のコミット

--- studyauthjs/src/app/page.tsx
+++ studyauthjs/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home(){
   const thisMonth = today.getMonth();
   const thisYear = today.getFullYear();
   //仮データ
-  const days = [1,2,3,4,5,6,7,8,9,10,
+  const days = [0,1,2,3,4,5,6,7,8,9,10,
                 11,12,13,14,15,16,17,18,19,20,
                 21,22,23,24,25,26,27,28,29,30];
   
@@ -22,19 +22,26 @@ export default function Home(){
     </div>
 
     <h1 className="logo-font text-4xl font-extrabold tracking-widest bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-400 text-transparent bg-clip-text drop-shadow-lg text-center my-6">
-      シェアカレンダー
+      時差計算シェアカレンダー
     </h1>
     <table className="w-full max-w-3xl mx-auto mt-6 border-separate border-spacing-2 backdrop-blur-sm border border-white/20 rounded-xl overflow-hidden shadow-xl">
       <tbody>
-        <tr className="text-center">
-          {days.map((day)=>(
-            <td className="galaxy-input galaxy-td">
+          {days.map((day, i)=>(
+            <>
+            
+            {i !== 0 && i % 7 === 0 ?
+            <tr>
+              <td className="galaxy-input galaxy-td">
+              {day + "改行"}
+              </td>
+
+            </tr> : <td className="galaxy-input galaxy-td">
               {day}
-            </td>
-          ))}
-        </tr>
+              </td>}
+            </>
+        ))}
       </tbody>
-      </table>
+    </table>
     
     </>
   )
